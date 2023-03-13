6d7f249e67487c06396ffeac5cfdb2f2c1d34c83
LokeshGola <lokeshgola121@gmail.com>
2023-03-13 22:33:02 +0530

    portfolio

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId);
     const nav = document.getElementById(navId);
 
-    if(toggle && navId){
+    if(toggle && nav){
         toggle.addEventListener("click", ()=>{
             nav.classList.toggle("show");
         })
