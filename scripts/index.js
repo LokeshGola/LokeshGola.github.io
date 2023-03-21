8032fce177df2607174bdd614c6589e15a350c38
LokeshGola <lokeshgola121@gmail.com>
2023-03-21 09:28:43 +0530

    portfolio

--- scripts/index.js
+++ scripts/index.js
@@ -24,12 +24,34 @@ function linkAction(){
 }
 
 navLink.forEach( n => n.addEventListener('click', linkAction))
+
 /*=== Scroll Reveal Animation === */
+const sr = ScrollReveal({
+    origin:'top',
+    distance: '80px',
+    duration:2000,
+    reset: true
+})
 
 /*=== Scroll Home === */
+sr.reveal('.home__title',{})
+sr.reveal('.button',{delay: 200})
+sr.reveal('.home-img',{delay: 400})
+sr.reveal('.home__social-icon',{interval: 200})
 
 /*=== Scroll About === */
+sr.reveal('.about__img',{})
+sr.reveal('.about__subtitle',{delay: 200})
+sr.reveal('.about__text',{delay: 400})
 
 /*=== Scroll Skills === */
+sr.reveal('.skills__subtitle',{})
+sr.reveal('.skills__text',{delay: 200})
+sr.reveal('.skills__data',{interval: 200})
+sr.reveal('.skills__img',{delay: 400})
 
 /*=== Scroll Work === */
+sr.reveal('.work__img', {interval:200})
+
+/*=== Scroll contact === */
+sr.reveal('.contact__input', {interval:200})
