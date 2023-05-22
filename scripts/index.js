90a147131b4a5dc48e825df22f4f83f3a9e0a376
LokeshGola <lokeshgola121@gmail.com>
2023-05-23 00:11:01 +0530

    portfolio

--- scripts/index.js
+++ scripts/index.js
@@ -25,8 +25,17 @@ function linkAction(){
 
 navLink.forEach( n => n.addEventListener('click', linkAction))
 
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
 /*=== Scroll Reveal Animation === */
-// co/g
+
 
 /*=== Scroll Home === */
 // sr.reveal('.home__title',{})
